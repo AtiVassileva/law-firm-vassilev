6c3949f291855a7d1b21b6c982ba152999394c35
Ati Vassileva <ativassileva@gmail.com>
2022-03-05 22:07:19 +0200

    Added about page

--- vassilev-law-firm/src/App.js
+++ vassilev-law-firm/src/App.js
@@ -1,6 +1,7 @@
 import { Switch, Route } from 'react-router-dom';
 import NavMenu from './components/NavMenu';
 import Home from './components/Home';
+import About from './components/About';
 import Footer from './components/Footer';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <NavMenu />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
       </Switch>
       <Footer/>
     </div>
