8176f22dc9cb7cf15004c3946fd5fc8b3138fb7f
Ati Vassileva <ativassileva@gmail.com>
2022-03-05 21:58:42 +0200

    Added navigation

--- vassilev-law-firm/src/App.js
+++ vassilev-law-firm/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import NavMenu from './components/NavMenu';
 import Home from './components/Home';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-    </Switch>
+    <div id="app">
+      <NavMenu />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </div>
   );
 }
 
---
+++ vassilev-law-firm/src/components/NavMenu.js
@@ -0,0 +1,31 @@
+﻿import { Link } from 'react-router-dom';
+
+const NavMenu = () => {
+    return (
+        <div className="nav-bar">
+            <div className="container-fluid">
+                <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+
+                    <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div className="navbar-nav mr-auto">
+                            <Link to="/" className="nav-item nav-link active">
+                                <i className="fa fa-home"></i>
+                                &nbsp;Начало
+                            </Link>
+                            <Link to="/about" className="nav-item nav-link">За мен</Link>
+                            <Link to="/practice" className="nav-item nav-link">Практика</Link>
+                            <Link to="/pricing" className="nav-item nav-link">Тарифи</Link>
+                            <Link to="/contact" className="nav-item nav-link">Контакти</Link>
+
+                        </div>
+                        <div className="ml-auto">
+                            <Link to="/contact" className="btn">Изпрати запитване</Link>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default NavMenu;
